e07eaf14f0bfe0e836545baa3c2d814a0b000bd3
prasanna <prasannakulkarni341@gmail.com>
2022-05-09 22:53:53 +0530

    py program FTMNNT

---
+++ desktop/msg.py
@@ -0,0 +1,16 @@
+import pyautogui as pt
+import time
+
+limit = input("enter limit:")
+message = input("message: ")
+i = 0
+time.sleep(10)
+
+while i<int(limit):
+    
+    pt.typewrite(message)
+    pt.press("enter")
+
+
+ 
+    i+=1
